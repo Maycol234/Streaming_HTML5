5771ffe65011b38f787bde3c51d462ada3458ec9
Maycol234 <kevinlopezacevedo905@gmail.com>
2026-04-16 12:44:31 -0600

    Clean up comments in cambiarPantalla function
    Removed comments for cleaner code.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function cambiarPantalla(id) {
-  // Ocultar todas las pantallas
+
   document.querySelectorAll('.pantalla').forEach(sec => sec.classList.remove('activa'));
-  // Mostrar la seleccionada
+
   document.getElementById(id).classList.add('activa');
 }
